3ceaa3dc734690fcb08fb53045f8ef9542df4fa3
Jonas Olmstead <sanojo@hotmail.com>
2017-04-21 16:09:02 +0200

    boilerplate phaser game

--- js/states/Game_create.js
+++ js/states/Game_create.js
@@ -4,5 +4,7 @@ var GameState = function(game) {
 
 GameState.prototype.create = function() {
 
+  this.game.add.image(0, 0, 'background');
+
   console.log('game started');
 };
--- public/js/index.js
+++ public/js/index.js
@@ -61,6 +61,8 @@ var GameState = function(game) {
 
 GameState.prototype.create = function() {
 
+  this.game.add.image(0, 0, 'background');
+
   console.log('game started');
 };
 
@@ -72,10 +74,14 @@ var SplashScreen = function(game) {};
 SplashScreen.prototype = {
 	preload: function() {
 		this.load.image('preloaderBar', 'assets/gfx/loading-bar.png');
+		this.load.image('splashBackground', 'assets/gfx/splashBackground.png');
+		this.load.image('background', 'assets/gfx/background.png');
 
 	},
 	create: function() {
     g_game.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    this.game.add.image(0, 0, 'splashBackground');
 
     console.log('waiting for space...');
   },
